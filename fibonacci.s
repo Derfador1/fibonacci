0697200c45355394764d1614caa3f5bdf1041947
Derfador1 <Derfador1@gmail.com>
2016-05-20 14:41:31 -0400

    changed my check for strol added comments drscribing the changes

--- fibonacci.s
+++ fibonacci.s
@@ -1,12 +1,5 @@
 .intel_syntax noprefix
 
-.data
-return_va:
-	.quad 0
-
-
-.text
-
 .input:
 	.string "0x%lx%lx%lx%lx\n"
 
@@ -24,21 +17,24 @@ main:
 	xor	rsi, rsi	#0 for the second argument
 	mov	rdx, 10		#10 for the last argument
 
-	mov	rsi, OFFSET return_va
 	call	strtol
-	mov	rsi, [return_va]
+
+	#move the value from strtol into my counter register
+	mov	rcx, rax
 
 	#check what strtol returns
-	cmp	BYTE PTR[rsi], 0
+	cmp	[rsi], byte ptr 0
 	jne	error
 
+	# check for greater than 300
 	cmp	rax, 300
 	jg	error
 
+	# check for less than 0
 	cmp	rax, 0
 	jl	error
 
-	mov	ecx, eax
+	#mov	ecx, eax
 	mov	r10, 1
 	# clearing the registers I will use to store my values
 	xor	r11, r11
@@ -52,23 +48,26 @@ main:
 #proper beginning implementation found with help of
 #https://github.com/pauliracane/fibonacci/blob/master/fibonacci.s
 fibonacci:
+	# check for if my value is 0 and jump to the finished part
+	cmp	rcx, 0
+	jle	done
 	# adds to my registers as the values overflows
 	add	r10, r11
 	adc	r12, r13
 	adc	r14, r15
 	adc	rbx, rbp
 
+	# swaps the values in the registers
 	xchg	r11, r10
 	xchg	r13, r12
 	xchg	r15, r14
 	xchg	rbp, rbx
 
 	# branches to fibonacci if value is not zero 
-	sub	ecx, 1
-	jne	fibonacci
-
-	#store the values for the 1st - 5th arguments for printf call
-	#prints from high to low bits
+	sub	rcx, 1
+	jmp	fibonacci
+done:
+	# store the values for the 1st - 5th arguments for printf call
 	mov	rdi, OFFSET .input
 	mov	rsi, rbp
 	mov	rdx, r15
@@ -77,13 +76,8 @@ fibonacci:
 	xor	eax, eax
 	call	printf
 	ret
-#fail:
-	#sets up the error string for this error
-	#mov	rdi, OFFSET .error
-	#call	puts
-	#ret
 error:
-	#sets up the error string for this error
+	# sets up the error string for this error
 	mov	rdi, OFFSET .error
 	call	puts
 	ret
