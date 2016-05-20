b37abf883a879955cd75b916bc378c4cd2cf0469
Derfador1 <Derfador1@gmail.com>
2016-05-20 13:05:16 -0400

    removed finished section to see if it would work without it

--- fibonacci.s
+++ fibonacci.s
@@ -1,8 +1,5 @@
 .intel_syntax noprefix
 
-.prompt:
-	.string "Please enter the desired Fibonacci Number (0-200):"
-
 .input:
 	.string "0x%lx%lx%lx%lx\n"
 
@@ -10,24 +7,28 @@
 	.asciz "Error improper about of arguments"
 
 .error1:
-	.asciz "Strtol error"
+	.asciz "Error with function call"
 
 .globl main
 main:
+	#check if there are onle 2 command line arguments, if not fail
 	cmp 	rdi, 2
 	jne	fail
 	
 	xor	eax, eax
-	mov	rdi, [rsi+8]	#argv[1]
-	xor	rsi, rsi	#0
-	mov	rdx, 10		#10
+	mov	rdi, [rsi+8]	#argv[1] for the first argument
+	xor	rsi, rsi	#0 for the second argument
+	mov	rdx, 10		#10 for the last argument
 	call	strtol
 
+	#check what strtol returns
+	#cmp	DWORD PTR[rsi], 0
 	cmp	rax, 0
 	je	error
 
 	mov	ecx, eax
 	mov	r10, 1
+	# clearing the registers I will use to store my values
 	xor	r11, r11
 	xor	r12, r12
 	xor	r13, r13
@@ -37,8 +38,9 @@ main:
 	xor	rbx, rbx
 
 #proper beginning implementation found with help of
-#https://github.com/dsprimm
+#https://github.com/dsprimm and 
 fibonacci:
+	# adds to my registers as the values overflows
 	add	r10, r11
 	adc	r12, r13
 	adc	r14, r15
@@ -49,10 +51,12 @@ fibonacci:
 	xchg	r15, r14
 	xchg	rbp, rbx
 
+	# branches to fibonacci if value is not zero 
 	sub	ecx, 1
 	jne	fibonacci
 
-
+	#store the values for the 1st - 5th arguments for printf call
+	#prints from high to low bits
 	mov	rdi, OFFSET .input
 	mov	rsi, rbp
 	mov	rdx, r15
@@ -61,27 +65,13 @@ fibonacci:
 	xor	eax, eax
 	call	printf
 	ret
-
-finished:
-	xor	rsi, rsi
-	xor	rdx, rdx
-	xor	rcx, rcx
-
-	mov	rdi, OFFSET .input
-	mov	rsi, rbx
-	mov	rdx, r14
-	mov	rcx, r12
-	mov	r8, r10
-	xor	eax, eax
-	call	printf
-	ret
-
-
 fail:
+	#sets up the error string for this error
 	mov	rdi, OFFSET .error
 	call	puts
 	ret
 error:
+	#sets up the error string for this error
 	mov	rdi, OFFSET .error1
 	call	puts
 	ret
