b118026bcf1b89c32c18669292ad4029875ffa55
Derfador1 <Derfador1@gmail.com>
2016-05-20 09:47:33 -0400

    currently working on printing the correct hex number

--- fibonacci.s
+++ fibonacci.s
@@ -4,7 +4,7 @@
 	.string "Please enter the desired Fibonacci Number (0-200):"
 
 .input:
-	.string "Ox%llx\n"
+	.string "Ox%x\n"
 
 .error:
 	.asciz "Error improper about of arguments"
@@ -26,25 +26,52 @@ main:
 	cmp	rax, 0
 	je	error
 
-	xor	r10, r10
+	xor	rbx, rbx
 	xor	r11, r11
 	xor	r12, r12
 	xor	r11, r13
 	xor	r12, r14
 	xor	r11, r15
 
-	mov	r10, 0x0
+	mov	rbx, 0x0
 	mov	r11, 0x1
 
 	mov	rcx, rax
 #proper beginning implementation found with help of
 #https://github.com/dsprimm
 fibonacci:
-	mov	r12, r10
+	mov	r12, rbx
 	add	r12, r11
-	mov	r10, r11
+	mov	rbx, r11
 	mov	r11, r12
-	loop	fibonacci
+
+	sub	ecx, 1
+	je	finished
+
+	sub	rcx, 1
+	jne	fibonacci
+
+	mov	rdi, OFFSET .input
+	mov	rsi, r15
+	mov	rdx, r13
+	mov	rcx, r11
+	xor	eax, eax
+	call	printf
+	ret
+
+finished:
+	xor	rsi, rsi
+	xor	rdx, rdx
+	xor	rcx, rcx
+
+	#mov	rdi, OFFSET .input
+	#mov	rsi, r15
+	#mov	rdx, r13
+	#mov	rcx, r11
+	#xor	eax, eax
+	#call	printf
+	#ret
+
 	sub	rsp, 8
 	mov	rdx, r12
 	mov	esi, OFFSET .input
